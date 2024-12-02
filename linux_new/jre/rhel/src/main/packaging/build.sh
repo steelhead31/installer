662f3f6e5bded6193f6a525cc28f419c864cc8cc
Scott Fryer <scfryer@redhat.com>
2024-12-02 16:59:13 +0000

    Test

--- linux_new/jre/rhel/src/main/packaging/build.sh
+++ linux_new/jre/rhel/src/main/packaging/build.sh
@@ -48,6 +48,7 @@ for spec in "$(ls /home/builder/build/generated/packaging/*.spec)"; do
 		# the sed command is to remove the trailing whitespace
 		# the second sed command is to replace %{arm} with armv7hl
 		ExclusiveArch=$(grep -E "^ExclusiveArch:" "$spec" | sed -e 's/ExclusiveArch: *//' | sed -e 's/%{arm}/armv7hl/g')
+		if [ "$ExclusiveArch" = "x64" ] ; then ExclusiveArch="x86_64" ; fi
 		[ -n "$ExclusiveArch" ] && targets="${ExclusiveArch}"
 	fi
 	for target in $targets; do
@@ -59,6 +60,9 @@ done;
 
 # Copy generated SRPMS, RPMs to destination folder
 find /home/builder/rpmbuild/SRPMS /home/builder/rpmbuild/RPMS -type f -name "*.rpm" -print0 | xargs -0 -I {} cp {} /home/builder/out
+# Rename Src RPM To Include Architecture
+cd /home/builder/out/ && for file in `ls -1 *.src.rpm`; do filename=`basename -s .src.rpm $file` ; SRCTarget="$filename.${buildArch}.src.rpm" ; mv $file $SRCTarget ; done && cd -
+
 # Sign generated RPMs with rpmsign.
 if grep -q %_gpg_name /home/builder/.rpmmacros; then
 	rm -f ~/.gnupg/public-keys.d/pubring.db.lock
